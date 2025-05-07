4aa9ec2d03992855fd60b468f891ba0381fe486b
Moussa <moussa.elbazioui@student.vives.be>
2025-05-08 00:01:39 +0200

    Restore real project content

--- src/hash.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-
-password = b"<Le1Z%-J8|~j"  # <- replace this with your actual password
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-print("Your bcrypt hash:", hashed.decode())
